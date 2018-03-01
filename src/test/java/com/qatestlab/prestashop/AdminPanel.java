21494e8c690381af92b345640b8af61fd2d6700d
Nepretest <nepretest@gmail.com>
2018-03-01 21:41:12 +0200

    Homework3 with main method

---
+++ src/test/java/com/qatestlab/prestashop/AdminPanel.java
@@ -0,0 +1,97 @@
+package com.qatestlab.prestashop;
+
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class AdminPanel {
+	
+	WebDriver driver;
+	Actions act;
+	Logger log;
+	
+	public AdminPanel(WebDriver driver, Actions act, Logger log) {
+		this.driver = driver;
+		this.act = act;
+		this.log = log;
+	}
+	
+	public void emailField(String email) {
+		log.info("Entering email :" + email);
+		WebElement element = driver.findElement(By.id("email"));
+		element.sendKeys(email);
+	}
+	
+	public void passwordfield(String password) {
+		log.info("Entering password :" + password);
+		WebElement element = driver.findElement(By.id("passwd"));
+		element.sendKeys(password);
+	}
+	
+	public void clickOnSubmitLogin() {
+		log.info("Clicking on submit login btn");
+		WebElement element = driver.findElement(By.name("submitLogin"));
+		element.click();
+		log.info("Submit login bnt is clicked");
+	}
+	
+//	public void clickOnCatalogue() {
+//		log.info("Clicking on Catalogue");
+//		WebElement element = driver.findElement(By.xpath("//li[@id='subtab-AdminCatalog']/a"));
+//		element.click();
+//		log.info("Catalogue is clicked");
+//		
+//	}
+
+	public void clickOnCategories() {
+		log.info("Clicking on Categories");
+		WebElement element = driver.findElement(By.xpath("//li[@id='subtab-AdminCategories']/a"));
+		element.click();
+		log.info("Categories is clicked");
+	}
+	
+	public void clickOnAddCategory() {
+		log.info("Clicking on Add Category btn");
+		WebElement element = driver.findElement(By.id("page-header-desc-category-new_category"));
+		element.click();
+		log.info("Add Category bnt is clicked");
+	}
+	
+	public void enterCategoryName(String name) {
+		log.info("Entering new category name");
+		WebElement element = driver.findElement(By.id("name_1"));
+		element.sendKeys(name);
+	}
+	
+	public void clickSaveCategory() {
+		log.info("Clicking on Save Category btn");
+		WebElement element = driver.findElement(By.id("category_form_submit_btn"));
+		element.click();
+		log.info("Save Category bnt is clicked");
+	}
+	
+	public WebElement findConfirmAlert() {
+		log.info("Finding confirmation alert");
+		WebElement element = driver.findElement(By.xpath("//div[@class='alert alert-success']"));
+		return element;
+		
+	}
+	
+	public void clickOnFilterCategoriesByName() {
+		log.info("Filter category list by name");
+		WebElement element = driver.findElement(By.xpath("//tr[@class='nodrag nodrop']/th[3]/span/a[1]"));
+		element.click();
+		log.info("Filter bnt is clicked");
+	}
+	
+	public WebElement findCreatedCategory(String name) {
+		log.info("Finding created category");
+		WebElement element = driver.findElement(By.xpath("//td[contains(text(),'" + name + "')]"));
+		return element;
+	}
+	
+	
+
+}
---
+++ src/test/java/com/qatestlab/prestashop/GenericMethods.java
@@ -0,0 +1,56 @@
+package com.qatestlab.prestashop;
+
+import java.util.Random;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class GenericMethods {
+	
+	Actions act;
+	Random rand = new Random();
+	WebDriver driver;
+	
+	public GenericMethods(WebDriver driver, Actions act) {
+		this.driver = driver;
+		this.act = act;
+	}
+	
+	public String randomName( ) {
+		String randName = "";
+		char[] letters = new char[] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+		for(int i = 0; i < 10; i++) {
+			char randLet = letters[rand.nextInt(letters.length)];
+			randName = randName + randLet;
+		}	
+		return randName;	
+	}
+	
+	public void moveToElementAct(String type, String locator) {
+		if(type.equalsIgnoreCase("xpath")) {
+			WebElement element = driver.findElement(By.xpath(locator));
+			act.moveToElement(element).perform();
+		} else if (type.equalsIgnoreCase("css")) {
+			WebElement element = driver.findElement(By.cssSelector(locator));
+			act.moveToElement(element).perform();
+		} else if (type.equalsIgnoreCase("id")) {
+			WebElement element = driver.findElement(By.id(locator));
+			act.moveToElement(element).perform();
+		} else if(type.equalsIgnoreCase("name")) {
+			WebElement element = driver.findElement(By.name(locator));
+			act.moveToElement(element).perform();
+		} else if(type.equalsIgnoreCase("class")) {
+			WebElement element = driver.findElement(By.className(locator));
+			act.moveToElement(element).perform();
+		} else if(type.equalsIgnoreCase("linktext")) {
+			WebElement element = driver.findElement(By.linkText(locator));
+			act.moveToElement(element).perform();
+		} else if(type.equalsIgnoreCase("partialLinkText")) {
+			WebElement element = driver.findElement(By.partialLinkText(locator));
+			act.moveToElement(element).perform();
+		}
+	}
+	
+}
